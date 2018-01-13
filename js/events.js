c3ef7ae40037240732adbfb4eced9e9a025fe1dc
rishabh2609 <rishabhchanana97@gmail.com>
2018-01-13 19:46:26 +0530

    event descp page

---
+++ js/events.js
@@ -0,0 +1,10 @@
+function myMap() {
+	var mapProp = {
+	  center: new google.maps.LatLng(51.508742,-0.120850),
+	  zoom: 5,
+	};
+	var map = new google.maps.Map(document.getElementById("google-map"), mapProp);
+}
+$(document).ready(function() {
+	
+});
