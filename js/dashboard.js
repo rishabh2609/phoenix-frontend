cba71ecb4f6cdf587c06d47a11c1a76f1835dab3
rishabh2609 <rishabhchanana97@gmail.com>
2018-01-13 21:53:27 +0530

    dashboard integrated

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,14 @@
+function myMap() {
+	var mapProp = {
+	  center: new google.maps.LatLng(51.508742,-0.120850),
+	  zoom: 5,
+	};
+	var map = new google.maps.Map(document.getElementById("google-map"), mapProp);
+}
 $(document).ready(function() {
 	$('#interested-btn').click(function() {
 		console.dir(this.parentElement.parentElement);
 	});
+	$('.modal').modal();
+	$('#create-event-modal').modal('open');
 });
